5075d2da333223ef582fedecd42db2e8e09bee99
David Kim <hyomithetech@gmail.com>
2020-01-16 12:46:11 -0800

    Change to functional component

--- js/components/Homepage.js
+++ js/components/Homepage.js
@@ -15,51 +15,45 @@ import {
   View
 } from "native-base";
 
-export default class HomePage extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Left />
-          <Body>
-            <Title>Tour AR</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <View style={{ alignItems: "center" }}>
-            <Image
-              style={{ width: 300, height: 200, justifyContent: "center" }}
-              source={require("/Users/EuiHyo_Mi/Desktop/MVP/client/js/components/image.png")}
-            ></Image>
-          </View>
-          <View>
-            <Button block light style={styles.button}>
-              <Text>Search</Text>
-            </Button>
-            <Button block light style={styles.button}>
-              <Text>Profile</Text>
-            </Button>
-            <Button block light style={styles.button}>
-              <Text>Create</Text>
-            </Button>
-          </View>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Log Out</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
-  }
-}
+const HomePage = () => {
+  return (
+    <Container>
+      <Header>
+        <Left />
+        <Body>
+          <Title>Tour AR</Title>
+        </Body>
+        <Right />
+      </Header>
+      <Content>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            style={{ width: 300, height: 200, justifyContent: "center" }}
+            source={require("../res/logo.png")}
+          ></Image>
+        </View>
+        <View>
+          <Button block light style={styles.button}>
+            <Text>Search</Text>
+          </Button>
+          <Button block light style={styles.button}>
+            <Text>Profile</Text>
+          </Button>
+          <Button block light style={styles.button}>
+            <Text>Create AR</Text>
+          </Button>
+        </View>
+      </Content>
+      <Footer>
+        <FooterTab>
+          <Button full>
+            <Text>Log Out</Text>
+          </Button>
+        </FooterTab>
+      </Footer>
+    </Container>
+  );
+};
 
 let styles = StyleSheet.create({
   buttonView: {
@@ -68,3 +62,5 @@ let styles = StyleSheet.create({
   },
   button: { marginTop: 60, marginHorizontal: 50, padding: 40 }
 });
+
+export default HomePage;
