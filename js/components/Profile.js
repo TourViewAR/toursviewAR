c7e58f116f9fb803cb0295b44dbfcb28e16b3808
Hunter Treadaway <huntertreadaway@Hunters-MacBook-Pro.local>
2020-01-16 12:05:14 -0800

    saving Profile progress

---
+++ js/components/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { ScrollView, View, StyleSheet } from 'react-native'
+
+import TourContainer from "./TourContainer";
+
+class Profile extends Component {
+  render () {
+    return (
+      <ScrollView>
+
+      </ScrollView>
+    )
+  }
+}
+
+export default Profile;
